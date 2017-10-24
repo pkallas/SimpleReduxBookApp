d5b15f9a82e9789a8943ff52f9f51a62bb42761c
pkallas <pkallas@Patricks-MacBook-Pro.local>
2017-10-24 15:12:21 -0700

    Fixed bug

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import bookList from '../containers/book-list';
+import BookList from '../containers/book-list';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        {bookList}
+        <BookList />
       </div>
     );
   }
